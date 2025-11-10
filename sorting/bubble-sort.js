48e835b9a6ead0060177c77d6a9e1a7479edd014
Ritesh Aggarwaal <35148160+rit359@users.noreply.github.com>
2025-11-10 10:47:22 +0530

    added decending sort logic

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,9 +1,25 @@
 let arr = [10,30,15,12,8];
 
+
+// logic for sorting in asending order
 for(let a=0; a <arr.length; a++){
   for(let i=0; i < (arr.length-a); i++){
       let temp = arr[i];
       if(arr[i] > arr[i+1]){
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+      }
+  }
+}
+
+console.log(arr);
+
+
+// logic for sorting in decending order
+for(let a=0; a <arr.length; a++){
+  for(let i=0; i < (arr.length-a); i++){
+      let temp = arr[i];
+      if(arr[i] < arr[i+1]){
         arr[i] = arr[i+1];
         arr[i+1] = temp;
       }
