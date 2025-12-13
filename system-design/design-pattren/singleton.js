7e0186ed1fd7c20d69d27b0cc0441b242218ca82
Ritesh Aggarwaal <35148160+rit359@users.noreply.github.com>
2025-12-13 20:24:24 +0530

    Update singleton.js

--- system-design/design-pattren/singleton.js
+++ system-design/design-pattren/singleton.js
@@ -2,24 +2,24 @@
 The Singleton Design Pattern is a creational design pattern that ensures only one instance of a class is created throughout the application and provides a global point of access to that instance.
 */
 
-class Config {
-  constructor() {
-    // if instance already exists, return it
-    if (Config.instance) {
-      return Config.instance;
+class ConfigManager {
+    private  static instacne:ConfigManager ;
+    
+    private ConfigManager(){
     }
-
-    // otherwise initialize
-    this.config = { appName: "Test" };
-
-    // store instance
-    Config.instance = this;
-  }
+    
+    public static getinstance():ConfigManager{
+        if(!ConfigManager.instacne){
+            this.instacne = new ConfigManager();
+        }
+        return ConfigManager.instacne;
+    }
+    
 }
 
-const config1 = new Config();
-const config2 = new Config();
+const ritesh = ConfigManager.getinstance();
+const vijay = ConfigManager.getinstance();
 
-console.log(config1 === config2); // true
-console.log(config1.config); // { appName: 'Test' }
+console.log(ritesh === vijay);
+
 
