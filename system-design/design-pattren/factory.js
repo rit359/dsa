61a1f7ecda3a1d4878b6d48550cd74b20c3efdf0
Ritesh Aggarwaal <35148160+rit359@users.noreply.github.com>
2025-12-13 20:22:54 +0530

    Update factory.js

--- system-design/design-pattren/factory.js
+++ system-design/design-pattren/factory.js
@@ -2,43 +2,37 @@
  Factory Design Pattern - is a creational design pattern that provides a way to create objects without exposing the creation logic to the client.
 Instead of using the new keyword directly, the client calls a factory method, which decides which subclass or object to create.
 */
-class Payment {
-  processPayment() {
-    throw new Error("processPayment() must be implemented");
-  }
+
+class Vichle {
+    move(){
+    } 
 }
 
-class CreditCard extends Payment {
-  processPayment() {
-    console.log("Doing payment using credit card");
-  }
-}
-
-class PayPal extends Payment {
-  processPayment() {
-    console.log("Doing payment using PayPal");
-  }
-}
-
-class PaymentFactory {
-  getPayment(type) {
-    if (type === "creditCard") {
-      return new CreditCard();
-    } else if (type === "paypal") {
-      return new PayPal();
-    } else {
-      throw new Error("Invalid payment type");
+class Car implements Vichle{
+     move(){
+         console.log("moving car");
     }
-  }
 }
 
+class Bike implements Vichle{
+     move(){
+         console.log("moving Bike");
+    }
+}
 
+class vichleFactory{
+    
+    static getVichle(type){
+        switch(type){
+            case 'car':
+                return new Car();
+            case 'bike':
+                return new Bike();
+            default:
+            return null;
+        }
+    }
+}
 
-// Usage
-const paymentFactory = new PaymentFactory();
-
-const payment1 = paymentFactory.getPayment("creditCard");
-payment1.processPayment(); // Doing payment using credit card
-
-const payment2 = paymentFactory.getPayment("paypal");
-payment2.processPayment(); // Doing payment using PayPal
+const vichle = vichleFactory.getVichle("bike");
+console.log(vichle.move());
