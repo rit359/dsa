2d10c10e9b4d5470c8d4823882697a579754b5fa
Ritesh Aggarwaal <35148160+rit359@users.noreply.github.com>
2025-12-14 10:25:23 +0530

    Update builder.js

--- system-design/design-pattren/builder.js
+++ system-design/design-pattren/builder.js
@@ -49,8 +49,45 @@ const burger = new BurgerBuilder().setSize("large").addTamato().addTopping().bui
 
 //output
 // Burger { size: 'large', cheese: false, tamato: true, topping: true }
-
-
-
-
 console.log(burger);
+
+
+//Example 2 by Pulkit
+
+class Pizza{
+    
+    private isTamato:boolean;
+    private isCheese:boolean;
+    private size: string;
+    
+    static Builder = class {
+        private isTamato: boolean = false;
+        private isCheese: boolean = false;
+        private size: string;
+        
+        constructor(size:string){
+            this.size = size;
+        }
+        
+        addCheese(isAdd){
+            this.isCheese = isAdd;
+            return this;
+        }
+        
+         addTamato(isAdd){
+            this.isTamato = isAdd;
+            return this;
+        }
+        
+        build(){
+            const pizza = new Pizza();
+            pizza.size = this.size;
+            pizza.isTamato = this.isTamato;
+            pizza.isCheese = this.isCheese;
+            return pizza;
+        }
+    }
+}
+
+const pizza = new Pizza.Builder('large').addCheese(true).addTamato(true).build();
+console.log(pizza);
